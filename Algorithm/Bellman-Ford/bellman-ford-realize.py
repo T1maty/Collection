90e232fe529d9b993b6b5080a62c554a94d251c9
T1maty <ttitaron@gmail.com>
2024-08-04 22:56:36 +0300

    Finish it

--- Algorithm/Bellman-Ford/bellman-ford-realize.py
+++ Algorithm/Bellman-Ford/bellman-ford-realize.py
@@ -24,5 +24,29 @@ class Graph:
        if distances[u] != float('inf') and distances[u] + weight < distances[v]:
           print("Graph contains a negative weight cycle ")
           return None
+
+
+     return distances
+
+
+
+if __name__ == "__main__":
+    g = Graph(5)
+    g.add_edge(0, 1, -1)
+    g.add_edge(0, 2, 4)
+    g.add_edge(1, 2, 3)
+    g.add_edge(1, 3, 2)
+    g.add_edge(1, 4, 2)
+    g.add_edge(3, 2, 5)
+    g.add_edge(3, 1, 1)
+    g.add_edge(4, 3, -3)
+
+    start_vertex = 0
+    distances = g.bellman_ford(start_vertex)
+
+    if distances:
+        print("distances from weight", start_vertex)
+        for vertex in range(g.vertices):
+            print(f"Weight {vertex}: {distances[vertex]}")
        
      
